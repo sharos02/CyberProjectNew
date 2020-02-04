4c595c23a55d5c54033b4d36f54778a2075242e0
sharos02 <58746256+sharos02@users.noreply.github.com>
2020-02-04 14:51:09 +0200

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private float x;
+    private float y;
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        transform.position += (Vector3)new Vector2(x * speed * Time.deltaTime, 0);
+        transform.position += (Vector3)new Vector2(0, y * speed * Time.deltaTime);
+    }
+
+}
---
+++ RoofCollide.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoofCollide : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "House_Roof")
+        {
+            Destroy(coll.gameObject);
+
+            
+            
+        }
+    }
+}
